de736a76a826b3ac54e60447f4b26989f2e0bebf
Mini01 <skekdwoals@naver.com>
2018-12-04 08:34:08 +0900

    추가

---
+++ src/com/biz/arrays/chars/StringChar07.java
@@ -0,0 +1,44 @@
+package com.biz.arrays.chars;
+
+public class StringChar07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 *  문자열 strNation을 char 배열로 바꾼후
+		 *  문자열을 거꾸로 콘솔에 출력하시오
+		 */
+		String strNation = "Republic of Korea";
+		
+		char[] charN = strNation.toCharArray();
+		char[] charR = new char[charN.length];
+		
+		
+		int intCount = charN.length;
+		for(char c : charN) {
+			charR[--intCount] = c;
+		}
+		for(int i = 0 ; i < charN.length; i++) {
+			int intR = charN.length - i-1;
+			charR[intR] = charN[i];
+		}
+		for(int i : charR) {
+			System.out.print((char)i + ", ");
+		}
+		System.out.println();
+		
+		// 역순이된 문자열로 재생성
+		String strRNation = new String(charR,0,charR.length);
+		System.out.println(strRNation);
+
+
+
+		}
+
+		
+		
+
+	}
+	
+
+
